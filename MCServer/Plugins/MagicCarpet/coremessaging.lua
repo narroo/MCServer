92fc6941d79868354bd7131c6f5306d8680b8218
Tycho <work.tycho+git@gmail.com>
2014-01-09 11:39:29 -0800

    Converted Magic carpet to GetPluginHandle API

--- MCServer/Plugins/MagicCarpet/coremessaging.lua
+++ MCServer/Plugins/MagicCarpet/coremessaging.lua
@@ -1,19 +1,22 @@
-Core = cPluginManager:Get():GetPlugin("Core")
+Core = cPluginManager:Get():GetPluginHandle("Core")
 
 function SendMessage(a_Player, a_Message)
-	if (Core ~= nil) then
+	if (Core ~= nil and Core.TryLoadLock()) then
 		Core:Call("SendMessage", a_Player, a_Message)
+		Core.ReleaseLoadLock()
 	end
 end
 
 function SendMessageSuccess(a_Player, a_Message)
-	if (Core ~= nil) then
+	if (Core ~= nil and Core.TryLoadLock()) then
 		Core:Call("SendMessageSuccess", a_Player, a_Message)
+		Core.ReleaseLoadLock()
 	end
 end
 
 function SendMessageFailure(a_Player, a_Message)
-	if (Core ~= nil) then
+	if (Core ~= nil and Core.TryLoadLock) then
 		Core:Call("SendMessageFailure", a_Player, a_Message)
+		Core.ReleaseLoadLock()
 	end
 end
